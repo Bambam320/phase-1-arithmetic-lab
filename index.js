de112832a67f9431e0aef58133f3a638eef55a37
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-01-18 01:05:18 -0500

    commit tree fc927189b7a27f974c4d5687d056912ef1b2112e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31 
+const num2 = 2
+const num3 = 54
+const num4 = 25
+let multiply = num1*num2;
+multiply;
+
+let random = Math.floor(Math.random() * 10) + 1;
+random;
+
+let mod = num3%num4;
+
+let max = Math.max(1, 20, 15, 9, 8, 14, 3, 19)
